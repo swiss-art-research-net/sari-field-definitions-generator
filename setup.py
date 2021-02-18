1b375fd6483435e7604449e0948460d10007be79
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-02-18 15:56:42 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="sari-field-definitions-generator", # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="Florian Kräutli",
     author_email="florian.kraeutli@uzh.ch",
     description="A generator for Field Definitions for ResearchSpace and Metaphacts",
-    install_requires=['pybars3','yaml'],
+    install_requires=['pybars3','PyYAML'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/swiss-art-research-net/sari-field-definitions-generator.git",
